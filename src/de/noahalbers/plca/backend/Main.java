33c68e2c86f6259e02e4a04e379a508fafd64247
Noah Albers <albers-noah@pm.me>
2021-03-08 15:05:26 +0100

    Added basic functionality for the telegram bot

--- src/de/noahalbers/plca/backend/Main.java
+++ src/de/noahalbers/plca/backend/Main.java
@@ -2,6 +2,12 @@ package de.noahalbers.plca.backend;
 
 import java.io.IOException;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import de.noahalbers.plca.backend.chatmessenger.TelegramBot;
+
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
@@ -20,5 +26,17 @@ public class Main {
 			e1.printStackTrace();
 			return;
 		}
+		
+		// TODO: Handle exceptions
+		try {
+			// Starts the telegram api
+			TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+			api.registerBot(new TelegramBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		System.out.println("Main stuff or smth");
 	}
 }
